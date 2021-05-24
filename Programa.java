f5d93ca76d70e1975a72cff051dc586a52ec24cb
andres <andres.lopera@upb.edu.co>
2021-05-23 21:09:59 -0500

    se comienza el programa

---
+++ Programa.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Programa{
+
+	//Clase para caracteres especiales.
+	public static String convertirUnicode(String letra,String cadena){
+		
+		StringBuilder str = new StringBuilder(cadena);
+		int indice = -1;
+		char caracter = 0;
+		indice = str.lastIndexOf(letra);
+		
+		if(indice>=0 )
+		{
+			switch(letra){
+				case "Á": caracter = '\u00C1';
+						  break;
+				case "á": caracter = '\u00E1';
+					      break;
+				case "é": caracter = '\u00E9';
+					      break;
+				case "í": caracter = '\u00ED';
+					      break;
+		        case "ó": caracter = '\u00F3';
+					      break;
+			    case "ú": caracter = '\u00FA';
+					      break;
+				case "ñ": caracter = '\u00F1';
+					      break;
+			}
+			str.replace(indice,indice+1,""+caracter);
+		}
+
+		return str.toString();
+	}
+
+	//Clase para imprimir los caracteres especiales.
+	public static void imprimir(String cadena)
+	{
+		String str; 
+		str = convertirUnicode("Á",cadena);
+		str = convertirUnicode("á",str);
+		str = convertirUnicode("é",str);
+		str = convertirUnicode("í",str);
+		str = convertirUnicode("ó",str);
+		str = convertirUnicode("ú",str);
+		str = convertirUnicode("ñ",str);
+
+		System.out.println(str);
+	}
